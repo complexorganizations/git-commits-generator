9c844781d0aa5d06a0d51d838ff940100d96db95
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-05-07 21:34:41 -0400

    Update main.go

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 var (
 	removeThisFile string
 	commitCount    int
+	err            error
 )
 
 func init() {
@@ -19,7 +20,7 @@ func init() {
 		log.Fatal("Error: The application git was not found in the system.")
 	}
 	tempCommitCount := flag.Int("commit", 10000, "The ammount of commits")
-	tempRemoveThisFile := flag.String("file", "removeThisFile", "The ammount of commits")
+	tempRemoveThisFile := flag.String("file", "remove-this-file", "The name of the file to remove")
 	flag.Parse()
 	commitCount = *tempCommitCount
 	removeThisFile = *tempRemoveThisFile
@@ -31,17 +32,27 @@ func main() {
 
 func generateCommits() {
 	for loop := 0; loop <= commitCount; loop++ {
-		os.WriteFile(removeThisFile, []byte(randomString(256)), 0644)
+		if fileExists(removeThisFile) {
+			err = os.Remove(removeThisFile)
+			handleErrors(err)
+		} else if !fileExists(removeThisFile) {
+			err = os.WriteFile(removeThisFile, []byte(randomString(256)), 0644)
+			handleErrors(err)
+		}
 		cmd := exec.Command("git", "add", removeThisFile)
-		cmd.Run()
+		err = cmd.Run()
+		handleErrors(err)
 		cmd = exec.Command("git", "commit", "-m", randomString(25))
-		cmd.Run()
+		err = cmd.Run()
+		handleErrors(err)
 		log.Println("Commit:", loop, "/", commitCount)
 	}
 	cmd := exec.Command("git", "pull")
-	cmd.Run()
+	err = cmd.Run()
+	handleErrors(err)
 	cmd = exec.Command("git", "push")
-	cmd.Run()
+	err = cmd.Run()
+	handleErrors(err)
 	generateCommits()
 }
 
@@ -59,4 +70,18 @@ func commandExists(cmd string) bool {
 	}
 	_ = appName
 	return true
+}
+
+func handleErrors(err error) {
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func fileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
 }
